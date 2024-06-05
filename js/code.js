df333a29b326786fcaef6bdc9d4b94068fadb829
Cristian <142132243+ant-shadow-wheel@users.noreply.github.com>
2024-06-05 17:54:48 -0500

    update code for mapv1

--- js/code.js
+++ js/code.js
@@ -1,12 +1,14 @@
 /* global document, window, fetch, cytoscape */
 const background = new Image();
+const grid20 = new Image();
+const inkarnatemap = new Image();
 var toJson = function (res) { return res.json(); };
 
 background.onload = () => {
 const cy = window.cy = cytoscape({
     container: document.getElementById('cy'),
-    elements: fetch('./assets/data.json').then(toJson),
-    style: fetch('./assets/cy-style.json').then(toJson),
+    elements: fetch('../assets/data.json').then(toJson),
+    style: fetch('../assets/cy-style.json').then(toJson),
 
     layout: {
         name: "preset",
@@ -15,13 +17,17 @@ const cy = window.cy = cytoscape({
     zoom: 2,
     pan: {x:0,y:0},
     minZoom: 1,
-    maxZoom: 5,
+    maxZoom: 8,
     autoungrabify: true
 });
 
 var content2var = "";
+var circuitid = "";
 
 cy.on('tap', 'node', function(){
+    cy.elements('edge[id ^= "lo"]').removeClass("yesvisible");
+    cy.elements('edge[id ^= "lo"]').removeClass("nonvisible");
+    cy.elements('edge[id ^= "lo"]').addClass("nonvisible");
     document.getElementById("desctitle").textContent = this.data('title');
     document.getElementById("desccontent").textContent = this.data('content1');
     content2var = this.data('content2');
@@ -32,6 +38,10 @@ cy.on('tap', 'node', function(){
     }
     else {
         document.getElementById("descexternal").textContent = content2var;
+    }
+    if (this.connectedEdges().size() >0){
+        circuitid='edge[id ^="' + this.connectedEdges()[0].data('id').substring(0,3) + '"]';
+        cy.elements(circuitid).addClass("yesvisible");
     }
 });
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -49,17 +59,23 @@ cy.on("render cyCanvas.resize", evt => {
     ctx.save();
     // Draw a background
     ctx.drawImage(background, 10, 10, 450, 240);
+    ctx.drawImage(inkarnatemap, 20, 10, 430, 220);
+    //ctx.drawImage(grid20, 10, 10, 450, 240);
 
     // Draw text that follows the model
-    ctx.font = "14px monospace";
+    ctx.font = "14px Verdana bold";
     ctx.fillStyle = "rgb(54,42,11)";
-    ctx.fillText("Veargreum timeline", 100, 224);
+    ctx.fillText("Veargreum timeline:", 10, 240);
 
     cy.nodes().forEach(node => {const pos = node.position();
         ctx.beginPath();
         ctx.fill();
     });
     ctx.restore();
+    cy.elements('edge[id ^= "lo"]').addClass("nonvisible");
 });
 }; // Preload image
-background.src = "./assets/parchmentGeneral.jpg";
+background.src = "../assets/parchmentGeneral.jpg";
+inkarnatemap.src = "../assets/parchmentCriusWide.png";
+//inkarnatemap.src = "../assets/criuswithtext.png";
+//grid20.src = "../assets/debuggrid20.png";
